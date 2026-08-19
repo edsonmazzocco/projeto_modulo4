324e402381534f1f7aa9a36f46c2680b36fa83fc
Edson Rodrigo Mazzocco <edsonmazzocco@gmail.com>
2026-08-19 20:10:04 -0300

    Finaliza a integração da tela de Login com API

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,23 +1,46 @@
+//imports de funções
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import  axios from "axios";
+
+//imports de estilos
 // MODO ANTIGO import "./Login.module.css";
 import styles from "./Login.module.css"; // MODO novo com CSS modules
 import stylesIndex from "../../index.module.css"; // MODO novo com CSS modules
-import { useState } from "react";
-import  axios from "axios";
+import swal from "sweetalert2";
+
 
 
 function Login() {
 
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-function fazerLogin(event) {
+//função para validar os dados na API
+async function fazerLogin(event: React.SubmitEvent) {
+  try {
     event.preventDefault();
-
-    axios.post("http://localhost:8888/auth/login", {
+    await axios.post("http://localhost:8888/auth/login", {
       email: email,
       senha: password,
     });
+    swal.fire({
+      title: "Login realizado com sucesso!",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 2000
+    });
+    navigate("/mesas");
+  } catch (error) {
+    swal.fire({
+      title: error.response.data.error,
+      icon: "error",
+    });
   }
+
+}
 
   return (
   <form className={styles.container} onSubmit={fazerLogin}>
@@ -27,12 +50,12 @@ function fazerLogin(event) {
 
     <div className={stylesIndex.containerInput}>
       <label>E-mail</label>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required></input>
     </div>
 
     <div className={stylesIndex.containerInput}>
       <label>Senha</label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
     </div>
 
     <button>Entrar</button>
