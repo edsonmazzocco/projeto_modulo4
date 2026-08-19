c994a75120d2fa58362a4fd54cafc43d337bc277
Edson Rodrigo Mazzocco <edsonmazzocco@gmail.com>
2026-08-18 22:20:19 -0300

    Desenvolvimento da tela de Login e integração com a API

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,43 @@
+// MODO ANTIGO import "./Login.module.css";
+import styles from "./Login.module.css"; // MODO novo com CSS modules
+import stylesIndex from "../../index.module.css"; // MODO novo com CSS modules
+import { useState } from "react";
+import  axios from "axios";
+
 
 function Login() {
-  return <div>Login Page</div>;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+function fazerLogin(event) {
+    event.preventDefault();
+
+    axios.post("http://localhost:8888/auth/login", {
+      email: email,
+      senha: password,
+    });
+  }
+
+  return (
+  <form className={styles.container} onSubmit={fazerLogin}>
+    <span className={styles.logo}>🍽️</span>
+    <h1 className={styles.nome}>Sabor e Arte</h1>
+    <p className={styles.subtitulo}>Acesse o Painel do Restaurante</p>
+
+    <div className={stylesIndex.containerInput}>
+      <label>E-mail</label>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+    </div>
+
+    <div className={stylesIndex.containerInput}>
+      <label>Senha</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+    </div>
+
+    <button>Entrar</button>
+  </form>
+  );
 }
 
 export default Login;
